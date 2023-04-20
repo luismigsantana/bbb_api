246cde95dbead91c85c0727e9210ef6ad87329b4
luis.santana <luis.santana@g.globo>
2023-04-20 15:42:15 -0300

    Acrecentando campos ao model

--- models/candidatos.py
+++ models/candidatos.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, UniqueConstraint
 
 from db.datebase import Base
 
@@ -7,6 +7,8 @@ class Candidato(Base):
     __tablename__ = "candidatos"
 
     id: int = Column(Integer, primary_key=True, index=True)
-    nome: str = Column(String(100), nullable=False)
     email: str = Column(String(100), nullable=False)
-    avatar_url: int = Column(String(255), nullable=False)
+    nome: str = Column(String(100), nullable=False)
+    idade: int = Column(Integer, nullable=False)
+    sobre: str = Column(String(510), nullable=False)
+    avatar_url: str = Column(String(255), nullable=True)
--- schemas/candidato_base.py
+++ schemas/candidato_base.py
@@ -5,4 +5,6 @@ from typing import Optional
 class CandidatoBase(BaseModel):
     nome: str
     email: str
+    sobre: str
+    idade: int
     avatar_url: Optional[str] = Field(None)
